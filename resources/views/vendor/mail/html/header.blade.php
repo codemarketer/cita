8763d3ec972674fe92eb02807015bb697816f79d
Alfredo <alfredogilvidal@gmail.com>
2025-02-12 11:49:46 +0100

    email header logo changed

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,11 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/logo.png') }}" class="logo" alt="{{ config('app.name') }}">
+@if (trim($slot) === 'Laravel')
+<img src="https://clinicanyr.com/wp-content/uploads/2023/12/logo-clinica-nyr.png" style="height: auto; width: 300px;" alt="Clínica NYR Logo">
+@else
+{{ $slot }}
+@endif
 </a>
 </td>
 </tr>
